d25b750fdf2f5f77afc458152cdbb53c718580c0
VTD104 <118832725+VTD104@users.noreply.github.com>
2024-03-22 12:23:35 +0700

    Add files via upload

---
+++ 0950080043_Võ Tấn Đạt_Bài Tập Buổi 2/0950080043_Vo Tan Dat-bài tập buổi 2/Appbar/bài tập 5. actions/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+  
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+         appBar: AppBar(
+          title: Text("AppBar Title"),
+          actions: <Widget> [
+            IconButton(
+              icon: Icon(Icons.file_upload),
+              onPressed: () => {
+                print("Click on upload button")
+              },
+            ),
+            IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () => {
+                  print("Click on settings button")
+                }
+            ),
+            PopupMenuButton(
+              icon: Icon(Icons.share),
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 1,
+                  child: Text("Facebook"),
+
+                ),
+                PopupMenuItem(
+                  value: 2,
+                  child: Text("Instagram"),
+                ),
+              ],
+            )
+          ]
+      ),
+      body: Center(
+          child: Text(
+            'Flutter AppBar Tutorial',
+          )
+      ),
+    );
+
+  }
+}
+
